7040e4ae7a51f488cda4771a03173f411a679826
taichi uga <zim2uga@gmail.com>
2017-03-04 07:34:00 +0000

    Implementation of the validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'messages#index'
-  resources :messages , only: [:create]
+  resources :messages , except: [:index, :new]
 end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
