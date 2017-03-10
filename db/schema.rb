09312cdeea965a370b2886ab8861178d8291457a
taichi uga <zim2uga@gmail.com>
2017-03-10 09:45:07 +0000

    age ts

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303015119) do
+ActiveRecord::Schema.define(version: 20170310092514) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
-    t.string   "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
