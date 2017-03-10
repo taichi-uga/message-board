30a2f9d517a3d118931619cb24a7d7a31f599a24
taichi uga <zim2uga@gmail.com>
2017-03-10 09:28:25 +0000

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20170303015119) do
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
+    t.string   "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
